6736762024623d9b2e3ff380ebea9acc8eac5187
Himasnhu <himanshubhartimax@gmail.com>
2024-02-07 02:36:09 +0530

    Update header section code and start footer section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import {BsSearch} from "react-icons/bs"
+import { BsSearch } from "react-icons/bs"
 
 const Header = () => {
   return (
@@ -31,15 +31,80 @@ const Header = () => {
             <div className="col-5">
               <div className="input-group">
                 <input type="text" className="form-control py-2 " placeholder="Search Your Products Here..." aria-label="Search Your Products Here..." aria-describedby="basic-addon2" />
-                  <span className="input-group-text p-3 " id="basic-addon2"><BsSearch className='fs-6' /></span>
+                <span className="input-group-text p-3 " id="basic-addon2"><BsSearch className='fs-6' /></span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between ">
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white '>
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className='mb-0'>Compare <br /> Products</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white '>
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className='mb-0'>Favourite  <br /> Wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white '>
+                    <img src="images/user.svg" alt="My Account" />
+                    <p className='mb-0'>Log in  <br /> My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white '>
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark '>0</span>
+                      <p className='mb-0'>₹499</p>
+                    </div>
+                  </Link>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
       </header>
       {/* Header Upper part code End Here */}
 
+
+      {/* Header Bottom part code Start */}
+      <header className='header-bottom py-3 '>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-botton d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src="images/menu.svg" alt="menu" />
+                      <span className='me-5 d-inline-block '>Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li><Link className="dropdown-item text-white " to="">Action</Link></li>
+                      <li><Link className="dropdown-item text-white " to="">Another action</Link></li>
+                      <li><Link className="dropdown-item text-white " to="">Something else here</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Header Bottom part code End Here */}
 
     </>
   )
